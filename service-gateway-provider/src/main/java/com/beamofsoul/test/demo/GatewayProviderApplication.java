86e79dc11759f591f4da2b329fc954e4437b883c
beamofsoul <beamofsoul@sina.com>
2022-08-23 15:22:44 +0800

    sentinel(flow control) when services calling each other

--- service-gateway-provider/src/main/java/com/beamofsoul/test/demo/GatewayProviderApplication.java
+++ service-gateway-provider/src/main/java/com/beamofsoul/test/demo/GatewayProviderApplication.java
@@ -3,7 +3,9 @@ package com.beamofsoul.test.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.beamofsoul.test.demo.*")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayProviderApplication {
--- service-gateway-provider/src/main/java/com/beamofsoul/test/demo/GatewayProviderController.java
+++ service-gateway-provider/src/main/java/com/beamofsoul/test/demo/GatewayProviderController.java
@@ -1,5 +1,6 @@
 package com.beamofsoul.test.demo;
 
+import com.beamofsoul.test.demo.feign.EchoFeign;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GatewayProviderController {
+
+    private EchoFeign echoFeign;
+
+    public GatewayProviderController(EchoFeign echoFeign) {
+        this.echoFeign = echoFeign;
+    }
 
     @GetMapping("/echo/{string}")
     public String echo(@PathVariable String string) {
@@ -23,5 +30,11 @@ public class GatewayProviderController {
     public String divide(@RequestParam Integer a, @RequestParam Integer b) {
         System.out.print("#");
         return String.valueOf(a / b);
+    }
+
+    @GetMapping("/internal/{string}")
+    public String internal(@PathVariable String string) {
+        String txt = this.echoFeign.echo(string);
+        return "Internal call: " + txt;
     }
 }
